70e9dfb3015c102201eb045a6255479033dd9d14
Damian Malysza <damian.malysza@gmail.com>
2020-10-19 02:16:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
   else
     new_array = []
     array.each_index do |index,name|
-      new_array[index] = "#{index+1}"
+      new_array[index] = "#{index+1}. #{name}"
     end
 end
 
